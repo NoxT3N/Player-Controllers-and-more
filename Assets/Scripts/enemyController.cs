b8422089575672599c3d7f77eed4572e67e76e36
NoxT3N <145358483+NoxT3N@users.noreply.github.com>
2023-10-09 15:56:11 +0100

    EnemySpawner

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyController : MonoBehaviour
-{
+public class EnemyController : MonoBehaviour
+{   
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float groundCheckDistance = .6f;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool movingRight = true;
+
+    [Header("Combat Settings")]
+    [SerializeField] private int maxHealth = 5;
+    private int currentHealth;
+    [SerializeField] private int damage = 1;
+
+    private Rigidbody2D enemyRigidBody;
+    private EnemySpawner spawner;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+    }
+
+    public void Initialize(EnemySpawner spawnerReference){
+       spawner = spawnerReference;
     }
 
     // Update is called once per frame
